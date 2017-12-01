d79ee103ef16ee2e6d5d74c4db40f77a252e1868
mib1092 <mazurenko.ihor@gmail.com>
2017-12-01 20:43:37 +0200

    mib

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function($) {
     //for burger menu
-    $('.mobile-menu-toggle, .mobile-menu-overlay').on('click', function () {
+    $('.mobile-menu-toggle, .mobile-menu-overlay, .mobile-menu a').on('click', function () {
         $('.mobile-menu-toggle').toggleClass('active');
         $('.mobile-menu-wrap').toggleClass('showing');
         $(document.body).toggleClass('overflow');
